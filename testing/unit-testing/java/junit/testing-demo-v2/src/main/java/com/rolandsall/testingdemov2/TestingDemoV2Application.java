0e7828b58091c54a7608e8a4b674200a4071e237
Roland Salloum <roland.salloum00@outlook.com>
2022-09-26 11:05:28 +0300

    chore: application dependencies and configuration setup

---
+++ testing/unit-testing/java/junit/testing-demo-v2/src/main/java/com/rolandsall/testingdemov2/TestingDemoV2Application.java
@@ -0,0 +1,13 @@
+package com.rolandsall.testingdemov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDemoV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingDemoV2Application.class, args);
+    }
+
+}
---
+++ testing/unit-testing/java/junit/testing-demo-v2/src/test/java/com/rolandsall/testingdemov2/TestingDemoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.testingdemov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingDemoV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
