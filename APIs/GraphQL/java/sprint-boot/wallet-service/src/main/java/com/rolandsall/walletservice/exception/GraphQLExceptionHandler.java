8a93e47183d10609b2877ca5f2fe1a9925897f2c
Roland Salloum <roland.salloum00@outlook.com>
2022-10-01 21:53:17 +0300

    feat: Error handling

---
+++ APIs/GraphQL/java/sprint-boot/wallet-service/src/main/java/com/rolandsall/walletservice/exception/GraphQLExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.rolandsall.walletservice.exception;
+
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        return new GraphQLError() {
+
+            @Override
+            public String getMessage() {
+                return ex.getMessage();
+            }
+
+            @Override
+            public List<SourceLocation> getLocations() {
+                return null;
+            }
+
+            @Override
+            public ErrorClassification getErrorType() {
+                return null;
+            }
+        };
+    }
+}
+
+
