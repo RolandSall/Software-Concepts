52216e2b68734757308f6c6561a0c588d9b3a343
RolandSall <roland.salloum00@outlook.com>
2022-11-08 22:38:57 +0200

    feat: adjusted project dependencies

--- microservices/microservice-full-example/microservices/src/test/java/com/rolandsall/AppTest.java
+++ microservices/microservice-full-example/microservices/src/test/java/com/rolandsall/AppTest.java
@@ -1,20 +1,20 @@
-package com.rolandsall;
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+//package com.rolandsall;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+///**
+// * Unit test for simple App.
+// */
+//public class AppTest
+//{
+//    /**
+//     * Rigorous Test :-)
+//     */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+//}
