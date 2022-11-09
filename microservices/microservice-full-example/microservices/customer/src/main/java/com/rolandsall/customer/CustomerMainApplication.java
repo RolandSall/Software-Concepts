ff5baf15dc0fc1266c93ad46c80f5f1b76f6afdb
RolandSall <roland.salloum00@outlook.com>
2022-11-09 14:24:28 +0200

    feat: add customer sub-module spring boot web

---
+++ microservices/microservice-full-example/microservices/customer/src/main/java/com/rolandsall/customer/CustomerMainApplication.java
@@ -0,0 +1,11 @@
+package com.rolandsall.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerMainApplication.class, args);
+    }
+}
