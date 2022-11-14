9f90905d7d9696bec3cd46cee28f7eb8f38a1046
RolandSall <roland.salloum00@outlook.com>
2022-11-14 23:44:06 +0200

    feat: distributed tracing using zipkin

--- microservices/microservice-full-example/microservices/fraud/src/main/java/com/rolandsall/fraud/api/FraudController.java
+++ microservices/microservice-full-example/microservices/fraud/src/main/java/com/rolandsall/fraud/api/FraudController.java
@@ -1,12 +1,14 @@
 package com.rolandsall.fraud.api;
 import com.rolandsall.client.fraud.FraudResponse;
 import com.rolandsall.fraud.services.IFraudCheckService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/fraud-check")
 public class FraudController {
 
@@ -18,6 +20,7 @@ public class FraudController {
 
     @GetMapping("{customerId}")
     public ResponseEntity<FraudResponse> CheckIfFraud(@PathVariable("customerId") UUID customerId) {
+        log.info(String.valueOf(customerId));
         boolean fraudCustomer = iFraudCheckService.isFraudCustomer(customerId);
         return ResponseEntity.ok().body(new FraudResponse(fraudCustomer));
 
